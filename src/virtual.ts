fde0cb0ea3c0599c34b9750e4dc209ad8f015acd
MMNN321 <moritz123456782@gmail.com>
2019-11-12 17:34:58 +0100

    Added new components to namespace

--- src/virtual.ts
+++ src/virtual.ts
@@ -1,15 +1,27 @@
 import Site from "./site";
 import Tool from "./tool";
+import Loader, { Script, Style } from "./v/loader";
 import Listener from "./v/listener";
 import ElementListener from "./v/elementListener";
-import ListenerLauncher from "./v/listenerLauncher";
+import ListenerLauncher, { ElementListenerCreation, BasicListenerCreation } from "./v/listenerLauncher";
 
 export default class Virtual {
     static Site             = Site;
     static Tool             = Tool;
+    static Loader           = Loader;
     static BasicListener    = Listener;
     static ElementListener  = ElementListener;
     static ListenerLauncher = ListenerLauncher;
+
+    static LoaderItem = {
+        Script,
+        Style
+    };
+
+    static ListenerLauncherItem = {
+        ElementListenerCreation,
+        BasicListenerCreation
+    };
 
     constructor() {
         throw new Error("Virtual namespace is not for constructing");
